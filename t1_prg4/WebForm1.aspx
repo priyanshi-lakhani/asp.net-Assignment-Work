0745cd076c4c413977e1525787d1b87d0fa92a9e
priyanshi-lakhani <priyanshil684@gmail.com>
2025-01-11 00:34:32 +0530

    Add files via upload

---
+++ t1_prg4/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="t1_prg4.WebForm1" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+            border: 4px double black; /* Double border for the table */
+        }
+        th, td {
+            border: 2px double black; /* Double border for table cells */
+            text-align: center;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h2 style="text-align:center;"></h2>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ t1_prg4/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace t1_prg4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        private void BindData()
+        {
+            // Create a DataTable to hold the data
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Salary");
+
+            // Add rows to the DataTable
+            dt.Rows.Add("100", "Rishab", "7000");
+            dt.Rows.Add("101", "Dharani", "7800");
+            dt.Rows.Add("102", "Joseph", "8500");
+            dt.Rows.Add("103", "Yamuna", "9500");
+
+            // Bind the DataTable to the GridView
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
+    
